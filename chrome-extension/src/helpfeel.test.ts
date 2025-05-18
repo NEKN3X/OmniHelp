5e77437bfc169fc94cdc43f7e9221d29659669bc
nekn3x <43030839+NEKN3X@users.noreply.github.com>
2025-05-18 16:44:47 +0900

    helpfeel記法の展開

--- chrome-extension/src/helpfeel.test.ts
+++ chrome-extension/src/helpfeel.test.ts
@@ -1,6 +1,32 @@
 import { expect, test } from 'vitest';
-import { expand } from './helpfeel';
+import { expand, literal, synonym } from './helpfeel';
+
+test('synonym', () => {
+  expect(synonym('abc')).toEqual([[['abc'], '']]);
+  expect(synonym('abc(xyz|123)')).toEqual([[['abc'], '(xyz|123)']]);
+  expect(synonym('abc(xyz|123)def')).toEqual([[['abc'], '(xyz|123)def']]);
+  expect(synonym('(xyz|123)')).toEqual([[['xyz', '123'], '']]);
+  expect(synonym('(xyz|123|あいう)abc')).toEqual([[['xyz', '123', 'あいう'], 'abc']]);
+});
+test('literal', () => {
+  expect(literal('abc')).toEqual([[['abc'], '']]);
+  expect(literal('(xyz)')).toEqual([[['xyz'], '']]);
+  expect(literal('((xyz))')).toEqual([[['xyz'], '']]);
+  expect(literal('(xyz|123)')).toEqual([[['xyz', '123'], '']]);
+  expect(literal('(xyz|123)abc')).toEqual([[['xyzabc', '123abc'], '']]);
+  expect(literal('abc(xyz|123|あいう)')).toEqual([[['abcxyz', 'abc123', 'abcあいう'], '']]);
+  expect(literal('abc(xyz|123)def')).toEqual([[['abcxyzdef', 'abc123def'], '']]);
+});
 
 test('expand', () => {
   expect(expand('abc')).toEqual(['abc']);
+  expect(expand('abc(xyz|123)')).toEqual(['abcxyz', 'abc123']);
+  expect(expand('abc(xyz|123)def')).toEqual(['abcxyzdef', 'abc123def']);
+  expect(expand('(xyz|123)')).toEqual(['xyz', '123']);
+  expect(expand('(xyz|123|あいう)')).toEqual(['xyz', '123', 'あいう']);
+  expect(expand('abc(xyz|123|あいう)')).toEqual(['abcxyz', 'abc123', 'abcあいう']);
+  expect(expand('abc(xyz|123)def')).toEqual(['abcxyzdef', 'abc123def']);
+  expect(expand('abc(xyz|123)def(abc|xyz)')).toEqual(['abcxyzdefabc', 'abcxyzdefxyz', 'abc123defabc', 'abc123defxyz']);
+  expect(() => expand('')).toThrow('Invalid input');
+  expect(() => expand('abc|xyz')).toThrow('Unused input');
 });
--- chrome-extension/src/helpfeel.ts
+++ chrome-extension/src/helpfeel.ts
@@ -1,3 +1,6 @@
+import { match, P } from 'ts-pattern';
+import { bind, orElse, parse, Parser, pure, sat, some, symbol } from './parser';
+
 // helpfeel記法を展開する
 // "? ログイン方法がわからない"
 // -> ["ログイン方法がわからない"]
@@ -24,6 +27,42 @@
 // 無視する記法
 // [word]: 検索の強化
 // (word1|:word2): 劣後
-export const expand = (input: string): string[] => {
-  return ['abc'];
+
+// 1. [[], 'xyz(abc|123|あいう)']
+// 2. [['xyzabc'], 'xyz(123|あいう)']
+// 3. [['xyzabc', 'xyz123'], 'xyz(あいう)']
+// 4. [['xyzabc', 'xyz123', 'xyzあいう'], '']
+
+const letter = sat(x => /[^\(\)\|]/.test(x));
+const str: Parser<string[]> = bind(some(letter), s => pure([s.join('')]));
+
+export const factor: Parser<string[]> = orElse(
+  bind(symbol('('), () => bind(synonym, syn => bind(symbol(')'), () => pure(syn)))),
+  str,
+);
+
+export const synonym: Parser<string[]> = bind(factor, f => {
+  return orElse(
+    bind(symbol('|'), () => bind(synonym, syn => pure(f.concat(syn)))),
+    pure(f),
+  );
+});
+
+export const literal: Parser<string[]> = bind(factor, f => {
+  return orElse(
+    bind(literal, lit => pure(f.flatMap(x => lit.map(y => x.concat(y))))),
+    pure(f),
+  );
+});
+
+export const expand = (input: string) => {
+  return match(parse(literal)(input))
+    .with([], () => {
+      throw new Error('Invalid input');
+    })
+    .with(P.array([P._, '']), ([[n, _]]) => n)
+    .with(P.array([P._, P.string]), ([[_, out]]) => {
+      throw new Error(`Unused input: ${out}`);
+    })
+    .exhaustive();
 };
